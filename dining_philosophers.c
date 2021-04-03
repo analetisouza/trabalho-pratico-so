7978ac172b44f3c860e390af4a11b56559ef0537
Ana Letícia Souza <analeticiarsouza@gmail.com>
2021-04-03 17:51:45 -0300

    first commit

---
+++ dining_philosophers.c
@@ -0,0 +1,25 @@
+/*
+
+Assumptions about the problem:
+
+- Philosophers start the dinner hungry
+- Philosophers spend a total time eating and a total time thinking
+- The dinner ends when they finish eating and thinking
+
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+void hungry(){}
+
+void eat(){}
+
+void think(){}
+
+int main (void) {
+
+    return 0;
+}
