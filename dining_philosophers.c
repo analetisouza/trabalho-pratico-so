c59460521dde59da7b738b187fd87e81dc2dd119
Ana Letícia Souza <analeticiarsouza@gmail.com>
2021-04-05 20:09:57 -0300

    main is basically ready

--- dining_philosophers.c
+++ dining_philosophers.c
@@ -1,6 +1,6 @@
 /*
 
-Assumptions about the problem:
+Assumptions about the problemvgzbvvazaz\ccc\zzcczdjmjbbbbnkeekwsk ekkkdjdmdmcfvvgffff  ddddz sss:
 
 - Philosophers start the dinner hungry
 - Philosophers spend a total time eating and a total time thinking
@@ -13,13 +13,51 @@ Assumptions about the problem:
 #include <pthread.h>
 #include <semaphore.h>
 
+#define THINKING 0
+#define HUNGRY 1
+#define EATING 2
+#define LEFT (n + 4) % 5
+#define RIGHT (n + 1) % 5
+
+sem_t chopstick[5];
+sem_t mutex;
+int state[5];
+int total_eating;
+int total_thinking; 
+
 void hungry(){}
+
+void take_chops(){} //test here
 
 void eat(){}
 
+void put_chops(){}
+
 void think(){}
+
+void* philosopher_actions(){} //copy total values
 
 int main (void) {
 
+    pthread_t philosopher[5];
+
+    printf("Type the amount of time each philosopher will spend eating: ");
+    scanf("%d", &total_eating);
+    printf("Type the amount of time each philosopher will spend thinking: ");
+    scanf("%d", &total_thinking);
+
+    // initiating chopsticks
+    for(int i = 0; i < 5; i++)
+        sem_init(&chopstick[i], 0, 1);
+
+    // initiating philosophers
+    for (int i = 0; i < 5; i++){
+        pthread_create(&philosopher[i], NULL, philosopher_actions, (void*)i);
+        state[i] = THINKING;
+    }
+
+    // joining philosophers threads
+    for(int i = 0; i < 5; i++)
+        pthread_join(philosopher[i], NULL);
     return 0;
 }
