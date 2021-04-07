31a853fd44b64ee9478870eca9827214f56ca0ad
Ana Letícia Souza <analeticiarsouza@gmail.com>
2021-04-06 21:40:56 -0300

    progress on getting chopsticks logic

--- dining_philosophers.c
+++ dining_philosophers.c
@@ -1,6 +1,6 @@
 /*
 
-Assumptions about the problemvgzbvvazaz\ccc\zzcczdjmjbbbbnkeekwsk ekkkdjdmdmcfvvgffff  ddddz sss:
+Assumptions about the problem:
 
 - Philosophers start the dinner hungry
 - Philosophers spend a total time eating and a total time thinking
@@ -16,8 +16,10 @@ Assumptions about the problemvgzbvvazaz\ccc\zzcczdjmjbbbbnkeekwsk ekkkdjdmdmcfvv
 #define THINKING 0
 #define HUNGRY 1
 #define EATING 2
-#define LEFT (n + 4) % 5
-#define RIGHT (n + 1) % 5
+#define LEFT_PHILOSOPHER (n + 4) % 5
+#define RIGHT_PHILOSOPHER (n + 1) % 5
+#define LEFT_CHOPSTICK n
+#define RIGHT_CHOPSTICK (n + 1) % 5
 
 sem_t chopstick[5];
 sem_t mutex;
@@ -25,20 +27,47 @@ int state[5];
 int total_eating;
 int total_thinking; 
 
-void hungry(){}
+void hungry(int n){
+    state[n] = HUNGRY;
+}
 
-void take_chops(){} //test here
+void take_chops(int n){
+    sem_wait(&mutex);
 
-void eat(){}
+    if (state[n] == HUNGRY && state[LEFT_PHILOSOPHER] != EATING && state[RIGHT_PHILOSOPHER] != EATING){
+        sem_wait(&chopstick[LEFT_CHOPSTICK]);
+        sleep(1);
+        sem_wait(&chopstick[RIGHT_CHOPSTICK]);
+    }
+        return;
+} //add busy chopsticks mechanism
+
+void eat(int n, int* eating){
+    state[n] = EATING;
+    sleep(1);
+    *eating = *eating - 1;
+}
 
 void put_chops(){}
 
 void think(){}
 
-void* philosopher_actions(){} //copy total values
+void* philosopher_actions(void* n){
+    int local_eating = total_eating;
+    int local_thinking = total_thinking;
+
+    while(local_eating > 0 && local_thinking > 0){
+        int* i = n;
+
+        hungry();
+        take_chops(i);
+        eat(i, &local_eating);
+        put_chops();
+        think();
+    }
+}
 
 int main (void) {
-
     pthread_t philosopher[5];
 
     printf("Type the amount of time each philosopher will spend eating: ");
